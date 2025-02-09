747704d70db5908d505213692c016a0a123de6c6
Worldtech21 <113043988+Worldtech21@users.noreply.github.com>
2025-02-09 22:08:44 +0530

    Create server.js

---
+++ server/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect("mongodb://localhost:27017/tasks", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const Task = mongoose.model("Task", new mongoose.Schema({ task: String }));
+
+app.get("/tasks", async (req, res) => {
+  const tasks = await Task.find();
+  res.json(tasks);
+});
+
+app.post("/tasks", async (req, res) => {
+  const newTask = new Task(req.body);
+  await newTask.save();
+  res.json(newTask);
+});
+
+app.delete("/tasks/:id", async (req, res) => {
+  await Task.findByIdAndDelete(req.params.id);
+  res.json({ message: "Task deleted" });
+});
+
+app.listen(5000, () => console.log("Server running on port 5000"));
